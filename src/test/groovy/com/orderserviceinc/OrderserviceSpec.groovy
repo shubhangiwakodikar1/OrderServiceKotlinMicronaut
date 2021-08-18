14f1ff459681c0f1f05b45f371db856d8547e9dd
Shubhangi Wakodikar <shubhangi.wakodikar@smartthings.com>
2021-08-17 19:18:23 -0500

    test

--- src/test/groovy/com/orderserviceinc/OrderserviceSpec.groovy
+++ src/test/groovy/com/orderserviceinc/OrderserviceSpec.groovy
@@ -1,8 +1,13 @@
 package com.orderserviceinc
 
+import com.orderserviceinc.models.OrdersRequest
+import com.orderserviceinc.models.OrdersResponse
+import com.orderserviceinc.services.OrderService
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
+import spock.lang.Unroll
+
 import javax.inject.Inject
 
 @MicronautTest
@@ -10,10 +15,36 @@ class OrderserviceSpec extends Specification {
 
     @Inject
     EmbeddedApplication<?> application
+    OrderService orderService
+    def transactionId
+    def message
+    OrdersRequest ordersRequest
 
+    def setup() {
+        orderService = new OrderService()
+        transactionId = UUID.randomUUID().toString()
+        message = "I love apples but not oranges"
+        ordersRequest = new OrdersRequest(message)
+    }
+
+    @Unroll
     void 'test it works'() {
         expect:
         application.running
+
+        when:
+        OrdersResponse ordersResponse = orderService.getOrderTotal(nApples, nOranges, ordersRequest, transactionId)
+
+        then:
+        ordersResponse.message == message
+        ordersResponse.NApples == nApples
+        ordersResponse.NOranges == nOranges
+        ordersResponse.orderTotal == ((OrderService.costOfApple * nApples) + (OrderService.costOfOrange * nOranges))
+
+        where:
+        nApples     |  nOranges
+        0           |  1
+        1           | -1
     }
 
 }
